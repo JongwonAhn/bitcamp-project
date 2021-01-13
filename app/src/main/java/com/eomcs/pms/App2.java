c92b81852c30ac4b11ee6664c09fb6a0856f8647
JongwonAhn <azsx1001202@naver.com>
2021-01-13 13:34:18 +0900

    v02

---
+++ app/src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,39 @@
+
+
+package com.eomcs.pms;
+
+public class App2 {
+  public static void main(String[] args) {    
+
+    System.out.println("[프로젝트]");
+    System.out.printf("번호: ");
+    System.out.println(1201);
+
+    System.out.print("프로젝트 명:");
+    System.out.println("미니 프로젝트 관리 시스템 개발");
+
+    System.out.print("내용");
+    System.out.println("소규모 팀을 위한 프로젝트 관리 시스템을 개발한다.");
+
+    System.out.print("시작일");
+    System.out.println("2020-01-01");
+
+    System.out.print("종료일");
+    System.out.println("2020-12-31");
+
+    System.out.print("만든이");
+    System.out.println("홍길동");
+
+    System.out.print("팀원" );
+    System.out.println("홍길동"+","
+        +"김구"+","
+        +"유관순"+","
+        +"안중근"+","
+        +"윤봉길");
+
+
+
+
+
+  }
+}
