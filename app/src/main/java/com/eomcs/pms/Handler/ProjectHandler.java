6a95830c17033b982e5c531576d51642889bd2cc
JongwonAhn <azsx1001202@naver.com>
2021-01-22 21:58:56 +0900

    .

---
+++ app/src/main/java/com/eomcs/pms/Handler/ProjectHandler.java
@@ -0,0 +1,71 @@
+package com.eomcs.pms.Handler;
+
+import com.eomcs.pms.domain.Project;
+import com.eomcs.util.Prompt;
+
+public class ProjectHandler {
+
+
+
+  static final int LENGTH = 100;
+  Project[] projects = new Project[LENGTH];
+  int size = 0;
+
+  public  void add(MemberHandler memberList) {
+    System.out.println("[프로젝트 등록]");
+
+    Project p = new Project();
+    p.no = Prompt.inputInt("번호? ");
+    p.title = Prompt.inputString("프로젝트명? ");
+    p.content = Prompt.inputString("내용? ");
+
+
+    p.endDate = Prompt.inputDate("종료일? ");
+
+
+    while(true) {
+      String name = Prompt.inputString("만든이?(취소: 빈문자열) ");
+      if(name.length() == 0) {
+        System.out.println("프로젝트 등록을 취소합니다.");
+        return;
+      } 
+      if(memberList.exist(name)) {
+        p.owner = name;
+        break;
+      }
+      System.out.println("등록된 회원이 아닙니다.");
+    }
+
+    p. members = "";
+    while(true) {
+      String name = Prompt.inputString("팀원?(완료: 빈문자열) ");
+
+      if(name.length() == 0) {
+        break;
+      }else if(memberList.exist(name)) {
+        if(!p.members.isEmpty()) {
+          p.members += ",";
+        }
+        p.members += name;
+      }else {
+        System.out.println("등록된 회원이 아닙니다.");        
+      }
+    }
+
+
+    projects[size++] = p;
+  }
+
+  public  void list() {
+    System.out.println("[프로젝트 목록]");
+
+    for (int i = 0; i < this.size; i++) {
+      Project p = this.projects[i];
+      System.out.printf("%d, %s, %s, %s, %s\n, [%s]\n ",
+          p.no, p.title, p.startDate, p.endDate, p.owner, p.members);
+    }
+  }
+
+
+
+}
